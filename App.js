66e448f9af721c1585eff3428f215b21661f8f96
Sidharth <sidharthvermaiiits@gmail.com>
2024-07-04 07:03:35 +0530

    Igniting our app

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
     // const heading = React.createElement("h1", {id:'heading'}, "Hello World from React");
     // 
     // root.render(heading)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent = React.createElement("div", {id: "parent"},
-    React.createElement("div", {id: "child"},
-        [React.createElement("h1",{}, "I am an H1 tag"),React.createElement("h2", {}, "I am an H2 tag")]
-    )
- )
- root.render(parent)
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [
+      React.createElement("h1", { key: "h1" }, "I am an H1 tag"),
+      React.createElement("h2", { key: "h2" }, "I am an H2 tag")
+    ]
+  )
+);
+
+root.render(parent);
+
+ // package.json is confuguration of npm
