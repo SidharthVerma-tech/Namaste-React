748fe6fee552b15d26ec4cb42480176df1d4f4d4
Sidharth <sidharthvermaiiits@gmail.com>
2024-07-04 08:54:49 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -4,21 +4,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// REACT elemnent  => Object => Render(HTML element)
+const heading = React.createElement("h1", {id : "heading"}, "Namaste React")
+
+// jsx => html or xml like syntax
+// jsx=> ReactCreateELement => ReactElement=>JSobject
+// bable transpiles the jsx code to js
+const number = 100;
+const jsxHeading  = (
+<h1>Namaste React using jsx</h1>
+)
+
+const newJSX = (
+    <h1 className='heading'>Namste Js</h1>
+)
+// React Component
+// React Functional Component is a functions which returns jsx element
+// title is react element , basically a javascript
+const title = (
+    <div id="title">
+        <h1>{number}</h1>
+        <h1> This is our title</h1>
+    </div>
+)
+const HeadingJSX = () => {
+    return (
+        <div id='container'>
+            {title}
+           <h1>Namaste React Functional Component</h1> 
+        </div>
+        
+    )
+}
+// Rendering one functional component into other is called functional composition in react
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [
-      React.createElement("h1", { key: "h1" }, "I am an H1 tag"),
-      React.createElement("h2", { key: "h2" }, "I am an H2 tag")
-    ]
-  )
-);
-
-root.render(parent);
-
- // package.json is confuguration of npm
+root.render(<HeadingJSX/>)
+// root.render(heading)
+// root.render(jsxHeading);
+// root.render(newJSX)
